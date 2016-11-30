1e64f52e1c70b4cf4d149bcac9c7c129932f0b0c
Matt Bettinson <mattbettinson@me.com>
2016-11-29 21:20:05 -0500

    Initial Commit

---
+++ ImageUploader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ImageUploader
+//
+//  Created by Matt Bettinson on 2016-11-29.
+//  Copyright © 2016 Matt Bettinson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
